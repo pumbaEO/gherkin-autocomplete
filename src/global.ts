83a05cbfb31e92184a4f80252425411c5c7b4062
Nikita Gryzlov <nixel2007@gmail.com>
2016-11-28 17:15:58 +0300

    Change property name

--- src/global.ts
+++ src/global.ts
@@ -64,7 +64,7 @@ export class Global {
 
         let pathsLibrarys: Array<string> =
             vscode.workspace.getConfiguration("gherkin-autocomplete")
-                .get<Array<string>>("featureLibrary", []);
+                .get<Array<string>>("featureLibraries", []);
         for (let i = 0; i < pathsLibrarys.length; ++i) {
             let library = pathsLibrarys[i];
             if (!(library.endsWith("/") || library.endsWith("\\"))) {
